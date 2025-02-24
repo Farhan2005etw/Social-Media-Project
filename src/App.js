10223cc1353e50add9122ff9a990b89fae444f0f
Farhan-2005-etw <farhan2005etw@gmail.com>
2025-02-24 08:36:02 +0530

    Add Components for Porject From Bootsrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Sidebar from './components/Sidebar'
+import Createpost from './components/Createpost';
+import Post from './components/Post';
+import PostList from './components/PostList';
 
 
 function App() {
-  return (
-    <>
-    <Header></Header>
+  return (    
+    <div className='app-container'>
     <Sidebar></Sidebar>
+    <div className="content">
+    <Header></Header>
+    <Createpost></Createpost>
+    <PostList></PostList>
     <Footer></Footer>
-    
-    </>
+    </div>
+    </div>   
   );
 }
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,7 @@ import React from "react";
 const Sidebar = () => {
     return(
         <>
-        <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style={{width: "280px"}}>
+        <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar" style={{width: "280px"}}>
     <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
       <svg className="bi pe-none me-2" width="40" height="32"><use xlinkhref="#bootstrap"></use></svg>
       <span className="fs-4">Sidebar</span>
@@ -19,27 +19,10 @@ const Sidebar = () => {
       <li>
         <a href="#" className="nav-link text-white">
           <svg className="bi pe-none me-2" width="16" height="16"><use xlinkhref="#speedometer2"></use></svg>
-          Dashboard
+          Create Post
         </a>
       </li>
-      <li>
-        <a href="#" className="nav-link text-white">
-          <svg className="bi pe-none me-2" width="16" height="16"><use xlinkhref="#table"></use></svg>
-          Orders
-        </a>
-      </li>
-      <li>
-        <a href="#" className="nav-link text-white">
-          <svg className="bi pe-none me-2" width="16" height="16"><use xlinkhref="#grid"></use></svg>
-          Products
-        </a>
-      </li>
-      <li>
-        <a href="#" className="nav-link text-white">
-          <svg className="bi pe-none me-2" width="16" height="16"><use xlinkhref="#people-circle"></use></svg>
-          Customers
-        </a>
-      </li>
+      
     </ul>
     <hr/>
     <div className="dropdown">
