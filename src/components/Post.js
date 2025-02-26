f517cc73164c84067663fbddeb20a8a59fd87d33
Farhan-2005-etw <farhan2005etw@gmail.com>
2025-02-26 22:59:15 +0530

    representing information to Post Vedio 11:07:42

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,26 @@
 import React from "react";
-const Post = () => {
+import { MdDelete } from "react-icons/md";
+
+
+const Post = ({post}) => {
     return(
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card post-card" style={{width: "30rem"}}>
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <h5 className="card-title">{post.title}
+
+    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+    <MdDelete />
+
+  </span>
+    </h5>
+    <p className="card-text">{post.body}</p>
+    {post.tags.map((tags) => (<span class="badge text-bg-primary tag">{tags}</span>))}
+    
   </div>
+
+  <div class="alert alert-success reaction" role="alert">
+  This post has been reacted by {post.reaction} people.
+</div>
 </div>
     );
 }
--- src/store/Post-list-store.js
+++ src/store/Post-list-store.js
@@ -1,6 +1,6 @@
 import { createContext, useReducer } from "react";
 
-const PostList = createContext({
+export const PostList = createContext({
     postList: [],
     addPost: () => {},
     deletePost: () => {}
@@ -13,7 +13,7 @@ const postListReducer = (currPostList, action) => {
 
 const PostListProvider = ( {children} ) => {
     
-    const [postList, dispatchPostList] = useReducer(postListReducer,[]);
+    const [postList, dispatchPostList] = useReducer(postListReducer, DEFAULT_POST_LIST);
 
 
     const addPost = () => {
@@ -34,5 +34,26 @@ const PostListProvider = ( {children} ) => {
     </PostList.Provider>
 
 };
+
+const DEFAULT_POST_LIST = [
+    {
+        id: "1", 
+        title: "Bhai Phati Padi h coding m",
+        body: "Sala compotition itna ho gya h ki samajh nhi aa rha Job lagegi ki nhi",
+        reaction: 2,
+        useId: "user-8",
+        tags: ["Job", "Coding", "Hard Work"]
+
+    },
+    {
+        id: "2", 
+        title: "Jo Hoga Dekha jayega Chill Kar Jani",
+        body: "Compotition h to kya hua kuch standout karke dikhana h",
+        reaction: 15,
+        useId: "user-10",
+        tags: ["Motivated", "Asli Hai"]
+
+    }
+]
 
 export default PostListProvider;
